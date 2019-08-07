276ca587a5dc6b5205fe2bcb076fc1e5c240b21f
julesThen <osilajules@gmail.com>
2019-08-07 19:32:02 +0800

    Add behavior to '#play-pause-btn > .logo' to change the logo when clicked at custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,21 @@ $(document).ready(function() {
   // Metronome components
   let BPMRANGESLIDER = $('#bpm-range-slider');
   let BPMINDICATOR = $('#bpm-indicator');
+  let PLAYPAUSEBTN = $('#play-pause-btn');
 
   // This changes the BPMINDICATOR's text according to BPMRANGESLIDER's value
   BPMRANGESLIDER.on('input', function(e) {
     let bpm = $(this).val();
 
     BPMINDICATOR.text(bpm);
+  });
+
+  // This plays/pauses the metronome
+  PLAYPAUSEBTN.click(function(e) {
+    let logo = $(this).children('.logo');
+
+    // for changing the logo
+    logo.toggleClass('glyphicon-play');
+    logo.toggleClass('glyphicon-pause');
   });
 });
