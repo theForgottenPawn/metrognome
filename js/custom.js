3cb940eb9abe9c3e553005b319a97e7e3b4f4e48
julesThen <osilajules@gmail.com>
2019-08-09 12:10:50 +0800

    Refactor metronome playing on custom.js

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,14 @@ $(document).ready(function() {
   let normal_beat = new Audio('audio/beat_01.mp3');
   let play_interval = null;
 
+  // Functions
+  function playMetronome() {
+    normal_beat.play();
+    play_interval = setInterval(function() { normal_beat.play(); },
+                                Math.round(60000/BPMRANGESLIDER.val()));
+  }
+
+  // Events listeners
   // This changes the BPMINDICATOR's text according to BPMRANGESLIDER's value
   BPMRANGESLIDER.on('input', function(e) {
     let bpm = $(this).val();
@@ -16,9 +24,7 @@ $(document).ready(function() {
 
     if (logo.hasClass('glyphicon-pause')) {
       clearInterval(play_interval);
-      normal_beat.play();
-      play_interval = setInterval(function() { normal_beat.play(); },
-                                  Math.round(60000/bpm));
+      playMetronome();
     }
   });
 
@@ -31,9 +37,7 @@ $(document).ready(function() {
     logo.toggleClass('glyphicon-pause');
 
     if (logo.hasClass('glyphicon-pause')) {
-      normal_beat.play();
-      play_interval = setInterval(function() { normal_beat.play(); },
-                                  Math.round(60000/BPMRANGESLIDER.val()));
+      playMetronome();
     } else if (logo.hasClass('glyphicon-play')) {
       clearInterval(play_interval);
     } else {
