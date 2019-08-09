074295c02219a020fab7c1aa328d0809394f3bd0
julesThen <osilajules@gmail.com>
2019-08-09 13:06:49 +0800

    Add behaviors to the bpm increase/decrease buttons on custom.js

--- js/custom.js
+++ js/custom.js
@@ -3,29 +3,49 @@ $(document).ready(function() {
   let BPMRANGESLIDER = $('#bpm-range-slider');
   let BPMINDICATOR = $('#bpm-indicator');
   let PLAYPAUSEBTN = $('#play-pause-btn');
+  let BPMINCREASEBTN = $('#bpm-increase-btn');
+  let BPMDECREASEBTN = $('#bpm-decrease-btn');
   // Variables
   let normal_beat = new Audio('audio/beat_01.mp3');
   let play_interval = null;
 
   // Functions
   function playMetronome() {
+    clearInterval(play_interval);
     normal_beat.play();
     play_interval = setInterval(function() { normal_beat.play(); },
                                 Math.round(60000/BPMRANGESLIDER.val()));
   }
 
+  function changeBpmEvenWhenPlaying() {
+    let logo = PLAYPAUSEBTN.children('.logo');
+
+    if (logo.hasClass('glyphicon-pause')) {
+      playMetronome();
+    }
+  }
+
+  function adjustBpmByOne(direction) {
+    let new_bpm = null;
+
+    if (direction === 'increase') {
+      new_bpm = Number.parseInt(BPMRANGESLIDER.val()) + 1;
+    } else if (direction === 'decrease') {
+      new_bpm = Number.parseInt(BPMRANGESLIDER.val()) - 1;
+    }
+
+    BPMRANGESLIDER.val(new_bpm);
+    BPMINDICATOR.text(new_bpm);
+    changeBpmEvenWhenPlaying();
+  }
+
   // Events listeners
   // This changes the BPMINDICATOR's text according to BPMRANGESLIDER's value
   BPMRANGESLIDER.on('input', function(e) {
-    let bpm = $(this).val();
-    let logo = PLAYPAUSEBTN.children('.logo');
+    // let bpm = $(this).val();
+    BPMINDICATOR.text($(this).val());
 
-    BPMINDICATOR.text(bpm);
-
-    if (logo.hasClass('glyphicon-pause')) {
-      clearInterval(play_interval);
-      playMetronome();
-    }
+    changeBpmEvenWhenPlaying();
   });
 
   // This plays/pauses the metronome
@@ -42,6 +62,18 @@ $(document).ready(function() {
       clearInterval(play_interval);
     } else {
       clearInterval(play_interval);
+    }
+  });
+
+  BPMINCREASEBTN.click(function() {
+    if (Number.parseInt(BPMRANGESLIDER.val()) < 260) {
+      adjustBpmByOne('increase');
+    }
+  });
+
+  BPMDECREASEBTN.click(function() {
+    if (Number.parseInt(BPMRANGESLIDER.val()) > 20) {
+      adjustBpmByOne('decrease');
     }
   });
 });
