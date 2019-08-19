30650d25a107e2695671fb669fb9dfc52b20a71e
theForgottenPawn <wakakaopanda@gmail.com>
2019-08-20 04:25:02 +0800

    Beats functionallity achieved but still needs some refactoring and debugging at custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
   // Start of Beats related code
   // Components
   const BEATSVISUAL = $('.beats-visual');
+  const EMPHASIZE1STBEAT = $('#emphasize-1st-beat');
   const BEATCOUNT = $('#beat-count');
 
   // Event handlers
@@ -29,13 +30,40 @@ $(document).ready(() => {
   const PLAYBUTTONLOGO = $('#play-pause-btn > .logo');
   // Variables
   const NORMALBEAT = new Audio('audio/beat_01.mp3');
+  const EMPHASISBEAT = new Audio('audio/beat_02.mp3');
+  let currentBeat = 0;
   let playInterval = null;
 
   // Functions
+  const playBeat = function playingTheBeat() {
+    currentBeat = currentBeat + 1;
+    let beatToPlay = NORMALBEAT;
+    let currentVisualBeat = BEATSVISUAL.find(`.beat:nth-child(${currentBeat})`);
+    
+    console.log(`${currentBeat} / ${BEATCOUNT.val()} `);
+    if (currentBeat === 1) {
+      BEATSVISUAL.find('.beat').removeClass('beat-played');
+      currentVisualBeat.addClass('beat-played');
+
+      if (EMPHASIZE1STBEAT[0].checked) {
+        beatToPlay = EMPHASISBEAT;
+      }
+    } else if (currentBeat === Number(BEATCOUNT.val(), 10)) {
+      currentVisualBeat.addClass('beat-played');
+      currentBeat = 0;
+    } else {
+      currentVisualBeat.addClass('beat-played');
+    }
+
+    beatToPlay.play();
+  };
+
   const playMetronome = function playTheMetronome() {
+    // It plays the metronome by resetting the 'playInterval' - indicating the
+    // sets of actions & decisions it must do every single time and seconds it
+    // takes before the said sets of instructions execute again.
     clearInterval(playInterval);
-    NORMALBEAT.play();
-    playInterval = setInterval(() => { NORMALBEAT.play(); },
+    playInterval = setInterval(() => { playBeat(); },
       Math.round(60000 / BPMRANGESLIDER.val()));
   };
 
