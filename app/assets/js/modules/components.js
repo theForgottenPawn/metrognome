022862d464ccb5f868fb0f8442c189622f6511dd
theForgottenPawn <wakakaopanda@gmail.com>
2020-09-02 09:46:46 +0800

    Add new module file 'components.js'

---
+++ app/assets/js/modules/components.js
@@ -0,0 +1,24 @@
+// Metronome
+export const BPMRANGESLIDER = $('#bpm-range-slider');
+export const BPMINDICATOR = $('#bpm-indicator');
+export const PLAYPAUSEBTN = $('#play-pause-btn');
+export const BPMINCREASEBTN = $('#bpm-increase-btn');
+export const BPMDECREASEBTN = $('#bpm-decrease-btn');
+export const PLAYBUTTONLOGO = $('#play-pause-btn > .logo');
+// Beats
+export const BEATSVISUAL = $('.beats-visual');
+export const EMPHASIZE1STBEAT = $('#emphasize-1st-beat');
+export const BEATCOUNT = $('#beat-count');
+// Notes Per Beat
+export const SINGLEBTN = $('#single-btn');
+export const TUPLETSBTN = $('#tuplets-btn');
+export const TRIPLETSBTN = $('#triplets-btn');
+export const TRIPLETSMIDRESTBTN = $('#triplets-mid-rest-btn');
+export const QUADRUPLETSBTN = $('#quadruplets-btn');
+// Timer
+export const ENABLE_TIMER_TOGGLER = $('#enable-timer-toggler');
+export const MIN_SETTER = $('#min-select');
+export const SEC_SETTER = $('#sec-select');
+export const TIME_RESETTER = $('#time-resetter');
+// Tap Timer
+export const TAP_TEMPO_BTN = $('#tap-tempo-btn');
